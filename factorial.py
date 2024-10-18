dbb6c67cbcd43b10163fc59114b06f33065babeb
Bhargav Ram <20BQ1A0458@vvit.net>
2024-10-18 09:54:56 +0530

    Updtaed for Webhooks functionlity testing

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 def factorial(n):
     if n < 0:
-        raise ValueError("Input must be a non-negative integer.")
+        raise ValueError("The input which u want to give  must be a non-negative integer.")
     if n == 0:
         return 1
     else:
